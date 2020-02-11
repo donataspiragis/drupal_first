1940ae5138f36555196652389ba900f99371f7ed
donataspiragis <donatas.piragis@gmail.com>
2020-02-11 19:02:34 +0200

    Module updated with block to customize

--- web/modules/custom/belzamo/src/Plugin/Block/SideBarer.php
+++ web/modules/custom/belzamo/src/Plugin/Block/SideBarer.php
@@ -23,18 +23,22 @@ class SideBarer extends BlockBase {
   public function build() {
     $config = $this->getConfiguration();
 
-    if (!empty($config['hello_block_settings'])) {
-      $name = $config['hello_block_settings'];
+    if (!empty($config['link'])) {
+      $link = $config['link'];
+      $text = strip_tags($config['text']);
+      $html = $config['html'];
     }
     else {
-      $name = $this->t('to no one');
+      $link = $this->t('to no one');
+      $text = $config['text'];
+      $html = $config['html'];
     }
     return array(
-      '#markup' => $this->t('Hello @name!', array(
-          '@name' => $name,
-        )
-      ),
-    );
+      '#theme' => 'belzamo_theme',
+      '#link' => $link,
+      '#text' => $text,
+      '#html' => $html,
+      );
   }
 
   /**
@@ -43,14 +47,27 @@ class SideBarer extends BlockBase {
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
 
-    $default_config = \Drupal::config('hello_world.settings');
+    $default_config = \Drupal::config('belzamo.settings');
     $config = $this->getConfiguration();
 
-    $form['hello_block_settings'] = array (
-      '#type' => 'textfield',
-      '#title' => $this->t('Who'),
-      '#description' => $this->t('Who do you want to say hello to?'),
-      '#default_value' => isset($config['hello_block_settings']) ? $config['hello_block_settings'] : $default_config->get('hello.name'),
+    $form['link'] = array (
+      '#type' => 'url',
+      '#title' => $this->t('Insert hyperlink'),
+      '#description' => $this->t('What link you want'),
+      '#default_value' => isset($config['link']) ? $config['link'] : $default_config->get('disc.link'),
+
+    );
+    $form['text'] = array (
+      '#type' => 'textarea',
+      '#title' => $this->t('Insert your Simple text'),
+      '#description' => $this->t('This text will be simple'),
+      '#default_value' => isset($config['text']) ? $config['text'] : $default_config->get('disc.text'),
+    );
+    $form['html'] = array (
+      '#type' => 'textarea',
+      '#title' => $this->t('Insert HTML'),
+      '#description' => $this->t('This text will show Html tags'),
+      '#default_value' => isset($config['html']) ? $config['html'] : $default_config->get('disc.html'),
     );
 
     return $form;
@@ -60,7 +77,9 @@ class SideBarer extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-    $this->setConfigurationValue('hello_block_settings', $form_state->getValue('hello_block_settings'));
+    $this->setConfigurationValue('link', $form_state->getValue('link'));
+    $this->setConfigurationValue('text', $form_state->getValue('text'));
+    $this->setConfigurationValue('html', $form_state->getValue('html'));
   }
 
   public function blockAccess(AccountInterface $account)
